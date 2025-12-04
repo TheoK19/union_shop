3bae781ea87a9c04170eee5e144676d7499a1acd
koripamotheo15 <koripamotheo15@gmail.com>
2025-12-04 18:48:42 +0000

    initialising firebase

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,11 @@ import 'package:union_shop/auth_page.dart';
 import 'package:union_shop/nav_bar.dart';
 import 'package:union_shop/print_shack_page.dart';
 import 'package:union_shop/print_shack_about_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const UnionShopApp());
 }
 
